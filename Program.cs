0ea949dbc51251b72c6ebde63c437f2a024b59fb
Boris <BorerKicker@yandex.ru>
2022-07-24 19:06:11 +0300

    created the iput method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+// Methods.
+int Input(string invite)
+{
+    Console.Write(invite);
+    return int.Parse(Console.ReadLine());
+}
+// My code.
+int length = Input("Введите размер массива: ");
+string[] arrayOfLines = new string[length];
+
